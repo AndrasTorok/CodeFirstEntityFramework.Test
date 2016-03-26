59e6b2a482dca6afa463f8a3d5bc41880b719127
Andras <andras.torok@yahoo.com>
2016-03-26 12:32:08 +0200

    first commit

---
+++ PersonRepositoryTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using CodeFirstEntityFramework.Repository;
+using CodeFirstEntityFramework.Model;
+
+namespace CodeFirstEntityFramework.Test
+{    
+    [TestClass]
+    public class PersonRepositoryTest
+    {
+        private ConnectionStringSettings css = ConfigurationManager.ConnectionStrings["CodeFirstEntityFramework"];
+        PersonRepository repo;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            repo = new PersonRepository(css);
+        }
+
+        [TestMethod]
+        public void Update_OK()
+        {
+            bool status = repo.Update(new Person { PersonId = 1, FirstName = "Peter2", LastName = "Gabriel" });
+
+            Assert.IsTrue(status);
+        }
+
+        [TestMethod]
+        public void Insert_OK()
+        {
+            bool status = repo.Save(new Person { FirstName = "Peter", LastName = "Gabriel" });
+
+            Assert.IsTrue(status);
+        }
+
+        [TestMethod]
+        public void Delete_OK()
+        {
+            bool status = repo.Delete(new object[] { 2 });
+
+            Assert.IsTrue(status);
+        }
+    }
+}
